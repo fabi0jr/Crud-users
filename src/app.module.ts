9dc6ff8192eec668dae9afd6bfb13efaf327ec83
fabi0jr <faelfjr35@gmail.com>
2025-04-23 10:04:12 -0400

    adicionar suporte ao MySQL, configurar conexão e criar serviços de banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,19 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module'; // Vamos criar isso depois
-import { User } from './user/entities/user.entity'; // Entidade do usuário
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
+      type: 'mysql', // Alterado para MySQL
+      host: '192.168.0.21', // IP da máquina virtual
+      port: 3306, // Porta padrão do MySQL
+      username: 'user', // Usuário do banco
+      password: 'user123', // Senha do banco
+      database: 'crud_users', // Nome do banco de dados
+      entities: [__dirname + '/**/*.entity{.ts,.js}'], // Caminho para as entidades
+      synchronize: false, // Desativar sincronização automática
     }),
     UserModule,
   ],
